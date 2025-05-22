1b78b7c961dc95b32a2847cc7179a6b953d82740
Jakub <s27345@pjwstk.edu.pl>
2025-05-22 21:53:54 +0200

    Added object values to DB

---
+++ WebApplication/WebApplication/Migrations/20250522195239_Values.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class Values : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "doctor@ihavegreatnamingconventions.com", "Doctor", "Doctor" });
+
+            migrationBuilder.InsertData(
+                table: "Medicaments",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[] { 1, "Take in case of a \"MAS\" induced headache", "Medicament", "PainKiller" });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "Patient@ihavegreatnamingconventions.com", "Patient", "Patient" });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 1, 1, "Amazing details here", 3 });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateOnly(2025, 5, 22), new DateOnly(2025, 6, 21), 1, 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "IdDoctor",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+        }
+    }
+}
