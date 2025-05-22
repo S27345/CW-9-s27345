30df72e3cbbe154880c3fea83f1afb56d411a955
Jakub <s27345@pjwstk.edu.pl>
2025-05-22 20:19:45 +0200

    Added ConnectionString and AppDbContext.cs

---
+++ WebApplication/WebApplication/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
