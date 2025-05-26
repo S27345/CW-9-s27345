77b4ca608fa6b27bd884b3208a3b317613b9eeb1
Jakub <s27345@pjwstk.edu.pl>
2025-05-26 19:46:15 +0200

    Added id validation

--- WebApplication/WebApplication/Controllers/PatientController.cs
+++ WebApplication/WebApplication/Controllers/PatientController.cs
@@ -11,6 +11,16 @@ public class PatientController(IDbService service) : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<IActionResult> Get(int id)
     {
+        var patients = await service.GetPatient(id);
+        if (!patients.Any())
+        {
+            return NotFound($"Patient not found with id {id}");
+        }
+        if (id <= 0)
+        {
+            return BadRequest("Invalid id. Has to be greater than 0");
+        }
+
         return Ok(await service.GetPatient(id));
     }
 }
